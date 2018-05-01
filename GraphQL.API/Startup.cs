3b22d22a0b07c4d42be7bb0afee7dd22a329f0f7
Reid Mewborne <mewborne@upward.org>
2018-04-30 22:19:46 -0400

    need to add migrations now.

--- GraphQL.API/Startup.cs
+++ GraphQL.API/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Graphql.Data.EF;
+using Graphql.Data.EF.Seed;
 using Graphql.Data.InMemory;
 using GraphQL.API.Models;
 using Grqphql.Core.data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,17 +32,24 @@ namespace GraphQL.API
             services.AddMvc();
             services.AddTransient<ReviewQuery>();
             services.AddTransient<IReviewsRepository, ReviewsRepository>();
+            services.AddDbContext<ReviewsContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionString:ReviewsDatabaseConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory, ReviewsContext db)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseMvc();
+
+            db.EnsureSeedData();
         }
     }
 }
---
+++ Graphql.Data/EF/Seed/ReviewsSeedData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Grqphql.Core.Models;
+
+namespace Graphql.Data.EF.Seed
+{
+    public static class ReviewsSeedData
+    {
+        public static void EnsureSeedData(this ReviewsContext db)
+        {
+            if (!db.Reviews.Any())
+            {
+                var review = new Review
+                {
+                    Body = "Graphql is awesome!",
+                    Id = 0,
+                    Rating = 5,
+                    User = "dropacake@me.com"
+                };
+                db.Reviews.Add(review);
+                db.SaveChangesAsync();
+            }
+        }
+    }
+}
